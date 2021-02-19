4be89b3e15508ed09cedc2441d8d0ae1e146f0cd
tharsis-soares <50808840+tharsis-soares@users.noreply.github.com>
2021-02-19 09:21:59 -0300

    setup emailing

--- controller/auth.js
+++ controller/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const ErrorResponse = require('../utils/errorResponse')
+const sendEmail = require('../utils/sendEmail')
 
 exports.register = async (req, res, next) => {
     const {username, email, password} = req.body
@@ -65,8 +66,24 @@ exports.forgotpassword = async (req, res, next) => {
             <p>por favor va para este link para refazer a senha</p>
             <a href=${resetUrl} clicktracking='off' >${resetUrl}</a>
             `
+
+            try {
+                await sendEmail({
+                    to: user.email,
+                    subject: 'Password Caramelo',
+                    text: message
+                })
+                res.status(200).json({success: true, data: "email enviado"})
+            }catch (error) {
+                user.resetPasswordToken = undefined
+                user.resetPasswordExpire = undefined
+                
+                await user.save()
+                return next(new ErrorResponse('email pode nao ter sido enviado', 500))
+            }
+
     } catch (error) {
-        
+        next(error)
     }
 }
 
